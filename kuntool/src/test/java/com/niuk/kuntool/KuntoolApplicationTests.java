5935328e18c00e0d4ce23beb724d6ac45bfcd16f
Niukun <1464042197@qq.com>
2023-09-08 10:34:42 +0800

    init project

---
+++ kuntool/src/test/java/com/niuk/kuntool/KuntoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niuk.kuntool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuntoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
